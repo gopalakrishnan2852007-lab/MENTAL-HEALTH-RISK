869aa81c747520728283e137e576ac9036799749
Gopalakrishnan <gopalakrishnan2852007@gmail.com>
2026-03-15 21:27:52 +0530

    o

--- mental health risk backend/backend/server.js
+++ mental health risk backend/backend/server.js
@@ -116,7 +116,7 @@ setInterval(async () => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../../mental health risk frontend/dist/index.html'));
 });
 
